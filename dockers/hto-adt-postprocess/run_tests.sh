96fbe1387c08f48af99708237c630e40888df5ff
Tobias Krause <tobiaspk1@gmail.com>
2024-08-29 18:42:00 -0400

    Update run_tests.sh

--- dockers/hto-adt-postprocess/run_tests.sh
+++ dockers/hto-adt-postprocess/run_tests.sh
@@ -2,7 +2,23 @@
 # using pyenv
 # python 3.12 (!)
 
-# setup
-pyenv virtualenv 3.9.13 sharp_test
+# install python 12
+pyenv install 3.12.5
+pyenv virtualenv 3.12.5 sharp_test
+
+# install requirements
 pyenv activate sharp_test
 pip install -r requirements.txt
+
+# run tests
+pytest -vvv --local
+
+# run ruff for linting
+pip install ruff
+pip install black
+
+ruff check --fix
+black *
+
+# uninstall environment
+pyenv uninstall sharp_test
